e6fedae145a9ba4797b90b7134cd30c694954a64
ronb <ronbnj@gmail.com>
2018-02-27 01:14:22 -0500

    Added carousel

--- dyr/resources/js/script.js
+++ dyr/resources/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     
-    /* Mobile navigation */
+    // Mobile navigation
     $('.js--nav-icon').click(function() {
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon .icon');
@@ -13,6 +13,26 @@ $(document).ready(function() {
         } else {
             icon.addClass('fa-bars');
             icon.removeClass('fa-times');
+        }
+    });
+    
+    // Slider carousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        center: true,
+        items: 1,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
         }
     });
 });

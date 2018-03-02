f0284c3576b5c9d64eb3302dfcbf8706894be6be
ronb <ronbnj@gmail.com>
2018-03-01 21:12:55 -0500

    Added carousel CSS rules and elements

--- dyr/resources/js/script.js
+++ dyr/resources/js/script.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
         var icon = $('.js--nav-icon .icon');
         
         nav.slideToggle(200);
-        console.log(icon);
+        
         if (icon.hasClass('fa-bars')) {
             icon.addClass('fa-times');
             icon.removeClass('fa-bars');
@@ -40,13 +40,16 @@ $(document).ready(function() {
         }
     });
     
-    // Slider carousel
-    $('.owl-carousel').owlCarousel({
+    // Slider carousels
+    var homeCarousel = $('.js--carousel-home');
+    
+    homeCarousel.owlCarousel({
+        dots: false,
+        stagePadding: 100,
         loop: true,
         center: true,
-        items: 1,
         margin: 10,
-        nav: true,
+        nav: false,
         responsive: {
             0: {
                 items: 1
@@ -55,9 +58,52 @@ $(document).ready(function() {
                 items: 2
             },
             1000: {
-                items: 4
+                items: 3
             }
         }
+    });
+    
+    var profileCarousel = $('.js--carousel-profile');
+    
+    profileCarousel.owlCarousel({
+        dots: false,
+        stagePadding: 100,
+        loop: true,
+        center: true,
+        items: 1,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    
+    $('.js--carousel-profile .center').css('background', 'red');
+    
+    // Home page carousel click events
+    $('.section-featured .arrow-left').click(function(){
+        homeCarousel.trigger('prev.owl.carousel');
+    });
+    
+    $('.section-featured .arrow-right').click(function(){
+        homeCarousel.trigger('next.owl.carousel');
+    });
+    
+    // Profile page carousel click events
+    $('.section-profile .arrow-left').click(function(){
+        profileCarousel.trigger('prev.owl.carousel');
+    });
+    
+    $('.section-profile .arrow-right').click(function(){
+        profileCarousel.trigger('next.owl.carousel');
     });
     
     $('.filter-menu').click(function() {
